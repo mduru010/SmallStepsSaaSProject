af36c5dec4386904af5d38b6ee6b90883cb3f3bc
Michael Duru <161900911+mduru010@users.noreply.github.com>
2024-07-12 18:45:47 -0500

    Basic Fucntionality

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+  "https://eyahhaybhihawrvscusc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YWhoYXliaGloYXdydnNjdXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1NTQ3MTMsImV4cCI6MjAzNjEzMDcxM30.9yHz9KnIJabS3QGEUhbBOHoZVRGOf8nvMNi9PQ0VBdA" 
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
 
